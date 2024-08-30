0a6d530ed656558dbb9e4d6fbe36db021a7f7728
9kalyan <rakalyanram@gmail.com>
2024-08-30 17:50:58 +0530

    kalyan ram

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import streamlit as st
-st.set_page_config(page_title="IMGGen",page_icon="❤️")
-st.title("kalyan ram")
-choice=st.sidebar.selectbox("Select your choice",["Home","DALL-E","Diffusers"])
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+from streamlit_toggle import toggle
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.title('Streamlit Toggle')
+toggle()
